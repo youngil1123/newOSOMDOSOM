bc67b6a8f88027932fd4043c40f4cbd4556ca829
jeongeunShim <sje11900@naver.com>
2023-02-11 11:59:37 +0900

    230211 mbti 페이지 레이아웃 수정전 ..(테스트 파일 패키지명 변경)

--- osomdosom/src/test/java/com/osom/OsomdosomApplicationTests.java
+++ osomdosom/src/test/java/com/osom/OsomdosomApplicationTests.java
@@ -1,13 +1,21 @@
 package com.osom;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.osom.service.MbtiService;
 
 @SpringBootTest
 class OsomdosomApplicationTests {
+	 @Autowired 
+	    MbtiService mservice;
 
-	@Test
-	void contextLoads() {
-	}
+	    @Test
+	    void contextLoads() {
+	    	int mbtimemno=mservice.countmbtimemno("isfp");
+	    	System.out.println(mbtimemno);
+		
+	    }
 
 }
--- osomdosom/src/test/java/com/osom/mbti/MbtiTests.java
+++ osomdosom/src/test/java/com/osom/mbti/MbtiTests.java
@@ -1,34 +1,30 @@
-package com.osom.controller;
+package com.osom.mbti;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import com.osom.dto.Mbti;
 import com.osom.service.MbtiService;
 
-@Controller
-public class MbtiController {
+@SpringBootTest
+class MbtiTests {
 
 	@Autowired
 	MbtiService mservice;
 
-	@RequestMapping("/mbtiex")
-    public String mbtiex(Model model, String mbti) {
-		//mbti 하나 페이지로 이동
-		System.out.println(mbti);
-		int mbtimemno = mservice.countmbtimemno(mbti);
-		model.addAttribute("mbtimemno", mbtimemno); // 현재 같은 mbti 회원수
-		
+	@Test
+	void contextLoads() {
+		int mbtimemno = mservice.countmbtimemno("isfp");
+		System.out.println(mbtimemno);
+
 		List<Mbti> conlist = new ArrayList<Mbti>();
-		conlist = mservice.getconnoAndcountmem(mbti); // mbti 별로 리뷰가 많은 컨텐츠 상위 10개의 컨텐츠 넘버  가져오기
+		conlist = mservice.getconnoAndcountmem("isfp");
 
 		List<Mbti> contents = new ArrayList<Mbti>();
-		
 		for (Mbti o : conlist) {
 			int con_no = o.getCon_no();
 			System.out.println(con_no);
@@ -65,17 +61,10 @@ public class MbtiController {
 			}
 
 		}
-		model.addAttribute("contents", contents);
-		
-		model.addAttribute("top", "mbtiex");
-		model.addAttribute("mbti", mbti);
-        return "/mbti/mbtiex";
-    }
-	
-	@RequestMapping("/mbtimain")
-    public String mbtimain(Model model) {
-		model.addAttribute("top", "mbtimain");
-		
-        return "/mbti/mbtimain";
-    }
+		for (Mbti s : contents) {
+			System.out.println("-------------------");
+			System.out.println(s);
+		}
+	}
+
 }
