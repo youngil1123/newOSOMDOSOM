f81bb80ac9ccd0abba6d5bac9c0c84c1140e07dd
jeongeunShim <sje11900@naver.com>
2023-02-13 10:55:47 +0900

    230213 미디어메인

--- osomdosom/src/main/java/com/osom/dto/Mbti.java
+++ osomdosom/src/main/java/com/osom/dto/Mbti.java
@@ -14,17 +14,22 @@ import lombok.ToString;
 public class Mbti {
 	private String mbti;//엠비티아이
 	private int con_no;//컨텐츠 넘버
-	private double avgstar_rate;//평균평점
 	private int mbtimemno; //MBTI별 이용자수 총 합
 	private int oneconmem_no; //한 컨텐츠에 리뷰를 쓴 해당 mbti 이용자 수
 	private String congenre;
 	//책정보
 	private int book_no;
 	private String bookImageURL;
+	private String bookname;
+	private int vol;
 	//영화정보
 	private String movieCd;
 	private String movieposter;
+	private String movieNm;
 	//연극정보
 	private String mt20id;
 	private String poster;
+	private String prfnm;
+	
+	private double conStar_rate;
 }
